e26a30096c90648ebecffc0547b5f7b9bb2a7236
TeoDiaz <tdiazsan@gmail.com>
2019-01-08 14:08:50 +0100

    Iteration 3 and 4 on file client

--- client.js
+++ client.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-router.get('/', (req, res, next) => {
-  console.log(res)
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.post("/", (req, res, next) => {
+  const { destination, body } = req.body;
+  axios
+    .post("http://teo_diaz_messageapp_1:3000/message", { destination, body })
+    .then(response => {
+      res.send(`${response.data}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("This is my first, 'Hello World'");
 });
+
+app.use("/message", require("./client"));
 
 app.listen(9001, () => {
   console.log("I'm ready on port 9001!");
