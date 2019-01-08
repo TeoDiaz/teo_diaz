a9d763ccffbcdeb1d88e374356851196f36ce015
TeoDiaz <tdiazsan@gmail.com>
2019-01-08 11:34:00 +0100

    Created express with Get Route with message & Dockerfile

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("This is my first, 'Hello World'");
+});
+
+app.listen(9001, () => {
+  console.log("I'm ready on port 9001!");
+});
