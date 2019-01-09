0165ca5d1079b50255910af89ae70bd61ec1f2c5
TeoDiaz <tdiazsan@gmail.com>
2019-01-09 17:48:16 +0100

    Modified Readme with API documentation

---
+++ README.md
@@ -0,0 +1,53 @@
+#MessageApp
+***
+####This is the definitive guide to use this amazing API
+ 
+Send all request to: 
+```
+http://<localhost:9001/messages>
+```
+*Methods:*
+
+```
+method: post
+Content-Type: 'application/json'
+request: 
+{
+  "destination": "STRING",
+  "body": "STRING"
+}
+
+Required: Yes
+Max length:
+  "destination" = 50 characters
+  "body" = 100 characters
+
+Simple Example: 
+
+{
+  "destination": "User1",
+  "body": "Hello World"
+}
+```
+
+*Response:*
+
+Status 200:
+```
+OK
+```
+Status 400:
+```
+You can't provide an empty field
+Numbers are not allowed
+Destination or Body fields missing
+Destination name or message text had exceed the length limit
+```
+Status 500:
+```
+Internal Server error
+```
+
+
+
+
--- validations.js
+++ validations.js
@@ -1,15 +1,22 @@
-const validated = ((req, res) => {
+const validated = (req, res) => {
   const { destination, body } = req.body;
   if (destination == "" || body == "") {
-   res.status(400).send("You can't provide an empty field ")
-   return false
-   }else if(destination == Number || body == Number){
-    res.status(400).send("Numbers are not allowed")
-     return false
-   }else{
-     return true
-   }
-})
+    res.status(400).send("You can't provide an empty field");
+    return false;
+  } else if (destination == Number || body == Number) {
+    res.status(400).send("Numbers are not allowed");
+    return false;
+  } else if (destination == undefined || body == undefined) {
+    res.status(400).send("Destination or Body fields missing");
+    return false;
+  } else if (destination.length > 50 || body.length > 100) {
+    res
+      .status(400)
+      .send("Destination name or message text had exceed the length limit");
+    return false;
+  } else {
+    return true;
+  }
+};
 
-
-module.exports = validated
+module.exports = validated;
