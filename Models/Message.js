7443c7bd3439e642c49016b431bf689ea1dc0feb
TeoDiaz <tdiazsan@gmail.com>
2019-01-10 12:04:52 +0100

    Create mongo image

--- Models/Message.js
+++ Models/Message.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-
+  destination: String,
+  body: String
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
+
 module.exports = Message;
--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const sendMessage = require("./sendMessage");
 const validated = require('./validations')
+const connection = require('./database')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+connection()
 app.get("/", (req, res) => {
   res.send("This is my first, 'Hello World'");
 });
 
 app.post("/messages", (req, res) => {
   if (validated(req,res)){
+    
     sendMessage(req)
       .then(response => res.status(200).send(`${response.data}`))
       .catch(err =>
-        res.status(err.response.status).send(`There was an ${err}`)
+        res.status(err.response.status).send(err)
       );
   }
 });
+const {PORT} = process.env
 
-app.listen(9001, () => {
-  console.log("I'm ready on port 9001!");
+app.listen(PORT, () => {
+  console.log(`I'm ready on port ${PORT}!`);
 });
 
