99a67f861cf38c68b6696f0cae2b3331ed48fa0a
TeoDiaz <tdiazsan@gmail.com>
2019-01-15 18:10:54 +0100

    Check connections db and set primary

--- src/database/connect.js
+++ src/database/connect.js
@@ -9,12 +9,14 @@ const connection = dbUrl => {
   return {
     primary: false,
     connected: true,
-    connecton: mongoose.createConnection(dbUrl, {
+    connection: mongoose.createConnection(dbUrl, {
       useNewUrlParser: true
     })
   };
 };
+
 let created = [];
+
 creatingConnection = () => {
   created = [connection(primaryDB), connection(replicaDB)];
   created[0].primary = true;
@@ -22,16 +24,43 @@ creatingConnection = () => {
 creatingConnection();
 setTimeout(function() {}, 0);
 
+created.forEach(connect => {
+  connect.connection.on("connected", () => {
+    console.log("Database is connected correctly");
+    console.log(connect.primary) 
+  });
+  
+  
+});
+
+const checkConnected = (primaryDB, replica) => {
+  primaryDB.connection.on("disconnected", () => {
+    primaryDB.connected = false;
+    if (primaryDB.primary) {
+      primaryDB.primary = false;
+      replica.primary = replica.connected;
+    }
+  });
+
+  primaryDB.connection.on("reconnected", () => {
+    primary.isActive = true;
+    primary.isPrimary = !replica.isPrimary;
+  });
+};
+
+checkConnected(created[0], created[1]);
+checkConnected(created[1], created[0]);
+
 module.exports = {
   connect: () => creatingConnection(),
   check: dbSelected => {
     let dbReturned;
     if (dbSelected == "primary") {
-      dbReturned = created.find(db => (db.primary == true));
+      dbReturned = created.find(db => db.primary == true);
     } else if (dbSelected == "replica") {
-      dbReturned = created.find(db => (db.primary == false));
+      dbReturned = created.find(db => db.primary == false);
     }
-   
-    return dbReturned.connecton;
+
+    return dbReturned.connection;
   }
 };
